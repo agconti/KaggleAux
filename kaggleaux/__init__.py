ab5832ed10df6ba9b867ef3279cbaca291dc35cc
agconti <agc11d@gmail.com>
2014-10-26 13:11:11 -0400

    feat(kaggleaux/__init__): updated to import all packages on load

---
+++ kaggleaux/__init__.py
@@ -0,0 +1,11 @@
+import dataframe
+import modeling
+import ploting
+import scoring
+import utils
+
+__all__ = ['dataframe',
+           'modeling',
+           'ploting',
+           'scoring',
+           'utils']
